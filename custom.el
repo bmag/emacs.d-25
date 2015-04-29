e7519cb17b669259f25b80ee3d088694508d1086
Bar Magal <barmagal@gmail.com>
2015-04-29 19:46:36 +0300

    Initial commit

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (anaconda-mode magit yasnippet flycheck ace-jump-mode window-purpose use-package smex paredit-everywhere company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,76 @@
+(defconst init-start-time (current-time))
+
+(setq inhibit-startup-screen t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(ido-mode)
+(setq ido-enable-flex-matching t)
+(show-paren-mode)
+(winner-mode)
+(windmove-default-keybindings)
+
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "http://melpa.org/packages/")))
+(package-initialize)
+
+(require 'use-package)
+
+(use-package ace-jump-mode
+  :bind (("C-c SPC" . ace-jump-mode)
+	 ("C-c C-SPC" . ace-jump-mode-pop-mark)))
+
+(use-package anaconda
+  :defer t
+  :init
+  (add-hook 'python-mode-hook #'anaconda-mode))
+
+(use-package company
+  :diminish company-mode
+  :functions company-complete-common-or-cycle
+  :defer 1
+  :config
+  (global-company-mode)
+  (define-key company-active-map (kbd "<tab>") #'company-complete-common-or-cycle)
+  (setq company-idle-delay 0.3))
+
+(use-package flycheck
+  :defer 1
+  :config
+  (global-flycheck-mode))
+
+(use-package magit :defer t)
+
+(use-package paredit
+  :diminish paredit-mode
+  :defer t
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'paredit-mode))
+
+(use-package paredit-everywhere
+  :defer t
+  :init
+  (add-hook 'prog-mode-hook #'paredit-everywhere-mode))
+
+(use-package smex
+  :bind (("M-x" . smex)
+	 ("M-X" . smex-major-mode-commands)))
+
+(use-package window-purpose
+  :defer 1
+  :config
+  (purpose-mode)
+  (push '("*Pp Macroexpand Output*" . general) purpose-user-name-purposes)
+  (purpose-compile-user-configuration))
+
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :defer 1
+  :config
+  (yas-global-mode))
+
+(defconst init-finish-time (current-time))
+(defconst init-total-time (time-subtract init-finish-time init-start-time))
+(message "Loaded init (%s seconds)" (time-to-seconds init-total-time))
